eefd3d0a554d44b83c2f84791a2424370a0fd1c2
pelumiformapp-commits <pelumiformapp@gmail.com>
2026-07-22 03:34:26 +0100

    auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,34 @@
 
+const loginBtn=document.getElementById("loginBtn");
+
+loginBtn.onclick=()=>{
+
+const username=document.getElementById("username").value.trim();
+
+const room=document.getElementById("room").value.trim();
+
+const password=document.getElementById("password").value.trim();
+
+if(username===""){
+
+alert("Enter your name");
+
+return;
+
+}
+
+if(password===""){
+
+alert("Enter password");
+
+return;
+
+}
+
+localStorage.setItem("username",username);
+
+localStorage.setItem("room",room||"Global");
+
+window.location.href="dashboard.html";
+
+};
