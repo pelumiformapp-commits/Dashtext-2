a622286cab2131dde01fdecd85df1d4668e82a46
pelumiformapp-commits <pelumiformapp@gmail.com>
2026-07-22 03:25:43 +0100

    firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,24 @@
 
+const firebaseConfig = {
+
+apiKey:"YOUR_API_KEY",
+
+authDomain:"YOUR_PROJECT.firebaseapp.com",
+
+databaseURL:"https://YOUR_PROJECT-default-rtdb.firebaseio.com",
+
+projectId:"YOUR_PROJECT",
+
+storageBucket:"YOUR_PROJECT.appspot.com",
+
+messagingSenderId:"YOUR_ID",
+
+appId:"YOUR_APP_ID"
+
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth=firebase.auth();
+
+const db=firebase.database();
