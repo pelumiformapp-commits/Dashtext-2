8cb237e695048cfacdd3bf3e54bf7b1efba51ff2
pelumiformapp-commits <pelumiformapp@gmail.com>
2026-07-22 03:51:50 +0100

    firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,24 +1,24 @@
+// ===============================
+// DashText Firebase
+// Developed by Engr. Pelumi
+// ===============================
 
 const firebaseConfig = {
-
-apiKey:"YOUR_API_KEY",
-
-authDomain:"YOUR_PROJECT.firebaseapp.com",
-
-databaseURL:"https://YOUR_PROJECT-default-rtdb.firebaseio.com",
-
-projectId:"YOUR_PROJECT",
-
-storageBucket:"YOUR_PROJECT.appspot.com",
-
-messagingSenderId:"YOUR_ID",
-
-appId:"YOUR_APP_ID"
-
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-const auth=firebase.auth();
+// Realtime Database
+const db = firebase.database();
 
-const db=firebase.database();
+// References
+const usersRef = db.ref("users");
+const roomsRef = db.ref("rooms");
