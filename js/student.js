6098be04b175d70b651510b04dbfe779009825c6
pelumiformapp-commits <pelumiformapp@gmail.com>
2026-07-22 04:30:41 +0100

    student.js

--- js/student.js
+++ js/student.js
@@ -1,1 +1,48 @@
 
+document.getElementById("joinClassBtn").onclick=()=>{
+
+const code=prompt("Enter Class Code");
+
+if(code){
+
+alert("Joined Class: "+code);
+
+}
+
+};
+
+document.getElementById("notesBtn").onclick=()=>{
+
+alert("Study Notes coming soon.");
+
+};
+
+document.getElementById("assignmentBtn").onclick=()=>{
+
+alert("Assignments coming soon.");
+
+};
+
+document.getElementById("cbtBtn").onclick=()=>{
+
+window.location.href="cbt.html";
+
+};
+
+document.getElementById("resultBtn").onclick=()=>{
+
+alert("Results coming soon.");
+
+};
+
+document.getElementById("chatBtn").onclick=()=>{
+
+window.location.href="chat.html";
+
+};
+
+document.getElementById("liveBtn").onclick=()=>{
+
+alert("Live Class coming soon.");
+
+};
