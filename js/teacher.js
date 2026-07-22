da6221a528717c052f69912cfe634d40234c3aee
pelumiformapp-commits <pelumiformapp@gmail.com>
2026-07-22 04:26:57 +0100

    teacher.js

--- js/teacher.js
+++ js/teacher.js
@@ -1,1 +1,40 @@
 
+document.getElementById("createClassBtn").onclick=()=>{
+
+const name=prompt("Class Name");
+
+if(!name)return;
+
+alert("Class '"+name+"' created.");
+
+};
+
+document.getElementById("uploadNoteBtn").onclick=()=>{
+
+alert("Upload Notes feature coming soon.");
+
+};
+
+document.getElementById("assignmentBtn").onclick=()=>{
+
+alert("Assignment feature coming soon.");
+
+};
+
+document.getElementById("cbtBtn").onclick=()=>{
+
+window.location.href="cbt.html";
+
+};
+
+document.getElementById("liveBtn").onclick=()=>{
+
+alert("Live Class feature coming soon.");
+
+};
+
+document.getElementById("resultBtn").onclick=()=>{
+
+alert("Student Results feature coming soon.");
+
+};
